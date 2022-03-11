5889ae5c8cc77062ce044c1e21aeac9d35236c9d
MiryangJung <miryang.dev@gmail.com>
2022-03-11 20:42:02 +0900

    :sparkles: post

---
+++ pages/blog/[slug].tsx
@@ -0,0 +1,34 @@
+import Container from "components/Container";
+import { allPosts } from "contentlayer/generated";
+import { InferGetStaticPropsType } from "next";
+import { useMDXComponent } from "next-contentlayer/hooks";
+
+const Post = ({ post }: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const MDXComponent = useMDXComponent(post.body.code);
+  return (
+    <Container>
+      <div className="mt-10 prose">
+        <h1 className="text-sky-700">{post.title}</h1>
+        <MDXComponent />
+      </div>
+    </Container>
+  );
+};
+
+export const getStaticPaths = async () => {
+  return {
+    paths: allPosts.map((p) => ({ params: { slug: p._raw.flattenedPath } })),
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async ({ params }) => {
+  const post = allPosts.find((p) => p._raw.flattenedPath === params.slug);
+  return {
+    props: {
+      post,
+    },
+  };
+};
+
+export default Post;
